ea8e366751981901eb4af61e7324990f7575ff1d
matyhtf <mikan.tenny@gmail.com>
2013-10-14 16:02:13 +0800

    Fixbug:session不存在导致致命错误

---
+++ apps/configs/cache.php
@@ -0,0 +1,5 @@
+<?php
+$cache['session'] = array(
+  'type' => 'CMemcache',
+);
+return $cache;
--- libs/Swoole/Factory.php
+++ libs/Swoole/Factory.php
@@ -12,6 +12,9 @@ class Factory
         {
             $objectType = substr($func, 3);
             $config = \Swoole::getInstance()->config[strtolower($objectType)][$id];
+            if(empty($objectType) or empty($config['type'])){
+                throw new \Exception("config error.$objectType/$id not found");
+            }
             $class = '\\Swoole\\'.$objectType.'\\'.$config['type'];
             self::$pool[$id] = new $class($config);
         }

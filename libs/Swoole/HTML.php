6c664ed60c56297d27dbcda9f2770faa4babc654
weiwenhan <weiwenhan>
2014-07-10 23:13:06 -0700

    fix removeAttr bug

--- libs/Swoole/HTML.php
+++ libs/Swoole/HTML.php
@@ -105,7 +105,7 @@ class HTML
         {
             foreach($remove_attrs as $attr)
             {
-                $regx = '~<([a-z]+[^>]*)([\'"\s]+'.$attr.'\s?=\s?[\'"]?[^\'"\s]*[\'"]?)([^>]*)>~i';
+                $regx = '~<([a-z]+[^>]*)('.$attr.'[\s\t\r\n]*=[\s\t\r\n]*[\'"]?(?:.(?![\'">]|[\'"]?\S+\s*[\'"]?=))*.[\'"]?)([^>]*)>~i';
                 $html = preg_replace($regx,'<$1 $3>', $html);
             }
             return $html;

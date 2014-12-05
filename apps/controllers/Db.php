64ada1366c62bd55c0691e9dd802c1a1c5eb78c9
matyhtf <mikan.tenny@gmail.com>
2014-12-05 19:21:16 +0800

    增加示例

--- apps/controllers/Db.php
+++ apps/controllers/Db.php
@@ -25,4 +25,45 @@ class Db extends Swoole\Controller
         $id = $model->put(array('name' => 'swoole', 'level' => 5, 'mobile' => '19999990000'));
         echo "insert id = $id\n";
     }
+
+    function get()
+    {
+        $model = Model('User');
+        $user = $model->get(1);
+        /**
+         * 打印数组
+         */
+        var_dump($user->get());
+        /**
+         * 修改mobile 为 13800008888
+         */
+        $user->mobile = '13800008888';
+        $user->save();
+
+        //删除此条记录
+        //$user->delete();
+    }
+
+    function gets()
+    {
+        $model = Model('User');
+        //level = 5
+        $gets['level'] = 5;
+
+        //仅获取数据
+        //var_dump($model->gets($gets));
+
+        //分页
+        $gets['page'] = empty($_GET['page'])?1:intval($_GET['page']);
+        $gets['pagesize'] = 5;
+        $pager = null;
+        $list = $model->gets($gets, $pager);
+
+        foreach($list as $li)
+        {
+            echo "{$li['id']}: {$li['name']}<br/>\n";
+        }
+        //上一页/下一页
+        echo $pager->render();
+    }
 }

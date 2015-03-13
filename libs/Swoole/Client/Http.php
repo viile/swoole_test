a51be2d98f558a2dd17cdb6f3b0be2017cb984de
matyhtf <mikan.tenny@gmail.com>
2015-03-13 16:22:59 +0800

    修复BUG

--- libs/Swoole/Client/Http.php
+++ libs/Swoole/Client/Http.php
@@ -90,19 +90,25 @@ class Http
     function doRequest()
     {
         // Performs the actual HTTP request, returning true or false depending on outcome
-        if (!$fp = @fsockopen($this->host, $this->port, $errno, $errstr, $this->timeout)) {
+        if (!$fp = @fsockopen($this->host, $this->port, $errno, $errstr, $this->timeout))
+        {
             // Set error message
-            switch ($errno) {
+            switch ($errno)
+            {
                 case -3:
                     $this->errormsg = 'Socket creation failed (-3)';
+                    break;
                 case -4:
                     $this->errormsg = 'DNS lookup failure (-4)';
+                    break;
                 case -5:
                     $this->errormsg = 'Connection refused or timed out (-5)';
+                    break;
                 default:
                     $this->errormsg = 'Connection failed (' . $errno . ')';
                     $this->errormsg .= ' ' . $errstr;
                     $this->debug($this->errormsg);
+                    break;
             }
             return false;
         }
@@ -118,12 +124,15 @@ class Http
         $inHeaders = true;
         $atStart = true;
         // Now start reading back the response
-        while (!feof($fp)) {
+        while (!feof($fp))
+        {
             $line = fgets($fp, 4096);
-            if ($atStart) {
+            if ($atStart)
+            {
                 // Deal with first line of returned data
                 $atStart = false;
-                if (!preg_match('/HTTP\/(\\d\\.\\d)\\s*(\\d+)\\s*(.*)/', $line, $m)) {
+                if (!preg_match('/HTTP\/(\\d\\.\\d)\\s*(\\d+)\\s*(.*)/', $line, $m))
+                {
                     $this->errormsg = "Status code line invalid: " . htmlentities($line);
                     $this->debug($this->errormsg);
                     return false;
@@ -134,29 +143,39 @@ class Http
                 $this->debug(trim($line));
                 continue;
             }
-            if ($inHeaders) {
-                if (trim($line) == '') {
+            if ($inHeaders)
+            {
+                if (trim($line) == '')
+                {
                     $inHeaders = false;
                     $this->debug('Received Headers', $this->headers);
-                    if ($this->headers_only) {
+                    if ($this->headers_only)
+                    {
                         break; // Skip the rest of the input
                     }
                     continue;
                 }
-                if (!preg_match('/([^:]+):\\s*(.*)/', $line, $m)) {
+                if (!preg_match('/([^:]+):\\s*(.*)/', $line, $m))
+                {
                     // Skip to the next header
                     continue;
                 }
                 $key = strtolower(trim($m[1]));
                 $val = trim($m[2]);
                 // Deal with the possibility of multiple headers of same name
-                if (isset($this->headers[$key])) {
-                    if (is_array($this->headers[$key])) {
+                if (isset($this->headers[$key]))
+                {
+                    if (is_array($this->headers[$key]))
+                    {
                         $this->headers[$key][] = $val;
-                    } else {
+                    }
+                    else
+                    {
                         $this->headers[$key] = array($this->headers[$key], $val);
                     }
-                } else {
+                }
+                else
+                {
                     $this->headers[$key] = $val;
                 }
                 continue;
@@ -173,13 +192,17 @@ class Http
             $this->content = gzinflate($this->content);
         }
         // If $persist_cookies, deal with any cookies
-        if ($this->persist_cookies && isset($this->headers['set-cookie']) && $this->host == $this->cookie_host) {
+        if ($this->persist_cookies && isset($this->headers['set-cookie']) && $this->host == $this->cookie_host)
+        {
             $cookies = $this->headers['set-cookie'];
-            if (!is_array($cookies)) {
+            if (!is_array($cookies))
+            {
                 $cookies = array($cookies);
             }
-            foreach ($cookies as $cookie) {
-                if (preg_match('/([^=]+)=([^;]+);/', $cookie, $m)) {
+            foreach ($cookies as $cookie)
+            {
+                if (preg_match('/([^=]+)=([^;]+);/', $cookie, $m))
+                {
                     $this->cookies[$m[1]] = $m[2];
                 }
             }
@@ -187,13 +210,16 @@ class Http
             $this->cookie_host = $this->host;
         }
         // If $persist_referers, set the referer ready for the next request
-        if ($this->persist_referers) {
+        if ($this->persist_referers)
+        {
             $this->debug('Persisting referer: ' . $this->getRequestURL());
             $this->referer = $this->getRequestURL();
         }
         // Finally, if handle_redirects and a redirect is sent, do that
-        if ($this->handle_redirects) {
-            if (++$this->redirect_count >= $this->max_redirects) {
+        if ($this->handle_redirects)
+        {
+            if (++$this->redirect_count >= $this->max_redirects)
+            {
                 $this->errormsg = 'Number of redirects exceeded maximum (' . $this->max_redirects . ')';
                 $this->debug($this->errormsg);
                 $this->redirect_count = 0;
@@ -201,7 +227,8 @@ class Http
             }
             $location = isset($this->headers['location']) ? $this->headers['location'] : '';
             $uri = isset($this->headers['uri']) ? $this->headers['uri'] : '';
-            if ($location || $uri) {
+            if ($location or $uri)
+            {
                 $url = parse_url($location . $uri);
                 // This will FAIL if redirect is to a different site
                 return $this->get($url['path']);
@@ -213,31 +240,38 @@ class Http
     function buildRequest()
     {
         $headers = array();
-        $headers[] = "{$this->method} {$this->path} HTTP/1.0"; // Using 1.1 leads to all manner of problems, such as "chunked" encoding
+        //Using 1.1 leads to all manner of problems, such as "chunked" encoding
+        $headers[] = "{$this->method} {$this->path} HTTP/1.0";
         $headers[] = "Host: {$this->host}";
         $headers[] = "User-Agent: {$this->user_agent}";
         $headers[] = "Accept: {$this->accept}";
-        if ($this->use_gzip) {
+        if ($this->use_gzip)
+        {
             $headers[] = "Accept-encoding: {$this->accept_encoding}";
         }
         $headers[] = "Accept-language: {$this->accept_language}";
-        if ($this->referer) {
+        if ($this->referer)
+        {
             $headers[] = "Referer: {$this->referer}";
         }
         // Cookies
-        if ($this->cookies) {
+        if ($this->cookies)
+        {
             $cookie = 'Cookie: ';
-            foreach ($this->cookies as $key => $value) {
+            foreach ($this->cookies as $key => $value)
+            {
                 $cookie .= "$key=$value; ";
             }
             $headers[] = $cookie;
         }
         // Basic authentication
-        if ($this->username && $this->password) {
+        if ($this->username && $this->password)
+        {
             $headers[] = 'Authorization: BASIC ' . base64_encode($this->username . ':' . $this->password);
         }
         // If this is a POST, set the content type and length
-        if ($this->postdata) {
+        if ($this->postdata)
+        {
             $headers[] = 'Content-Type: application/x-www-form-urlencoded';
             $headers[] = 'Content-Length: ' . strlen($this->postdata);
         }
@@ -263,9 +297,12 @@ class Http
     function getHeader($header)
     {
         $header = strtolower($header);
-        if (isset($this->headers[$header])) {
+        if (isset($this->headers[$header]))
+        {
             return $this->headers[$header];
-        } else {
+        }
+        else
+        {
             return false;
         }
     }
@@ -283,7 +320,8 @@ class Http
     function getRequestURL()
     {
         $url = 'http://' . $this->host;
-        if ($this->port != 80) {
+        if ($this->port != 80)
+        {
             $url .= ':' . $this->port;
         }
         $url .= $this->path;
@@ -354,7 +392,7 @@ class Http
         {
             $path .= '?' . $bits['query'];
         }
-        $client = new Http($host, $port);
+        $client = new self($host, $port);
         if (!$client->get($path))
         {
             return false;
@@ -372,28 +410,34 @@ class Http
         $port = isset($bits['port']) ? $bits['port'] : 80;
         $path = isset($bits['path']) ? $bits['path'] : '/';
         $path .= "?{$bits['query']}";
-        $client = new HttpClient($host, $port);
-        if (!$client->post($path, $data)) {
+        $client = new self($host, $port);
+
+        if (!$client->post($path, $data))
+        {
             return false;
-        } else {
+        }
+        else
+        {
             return $client->getContent();
         }
     }
 
     function debug($msg, $object = false)
     {
-        if ($this->debug) {
-            print '<div style="border: 1px solid red; padding: 0.5em; margin: 0.5em;"><strong>HttpClient Debug:</strong> ' . $msg;
-            if ($object) {
+        if ($this->debug)
+        {
+            echo
+                '<div style="border: 1px solid red; padding: 0.5em; margin: 0.5em;"><strong>HttpClient Debug:</strong> '
+                . $msg;
+            if ($object)
+            {
                 ob_start();
                 print_r($object);
                 $content = htmlentities(ob_get_contents());
                 ob_end_clean();
-                print '<pre>' . $content . '</pre>';
+                echo '<pre>' . $content . '</pre>';
             }
-            print '</div>';
+            echo '</div>';
         }
     }
 }
-
-?>

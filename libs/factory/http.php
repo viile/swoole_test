f8e6ce13feb420d16bbd9ffa31eef07b441da511
matyhtf <mikan.tenny@gmail.com>
2015-05-05 15:09:25 +0800

    增加扩展内置HttpServer的支持

--- libs/factory/http.php
+++ libs/factory/http.php
@@ -3,6 +3,10 @@ if (defined('SWOOLE_SERVER'))
 {
     $http = new Swoole\Http\PWS();
 }
+elseif (defined('SWOOLE_HTTP_SERVER'))
+{
+    $http = new Swoole\Http\ExtServer();
+}
 else
 {
     $http = new Swoole\Http\LAMP();

fa8429831bc3f4d8259e51d9009bb7add9854fe6
matyhtf <mikan.tenny@gmail.com>
2015-08-20 14:48:29 +0800

    ArrayObject

--- libs/Swoole/ArrayObject.php
+++ libs/Swoole/ArrayObject.php
@@ -1,13 +1,71 @@
 <?php
 namespace Swoole;
 
-class ArrayObject implements \Countable
+class ArrayObject implements \ArrayAccess, \Serializable, \Countable, \Iterator
 {
     protected $array;
+    protected $index = 0;
 
     function __construct($array)
     {
         $this->array = $array;
+    }
+
+    function current()
+    {
+        return current($this->array);
+    }
+
+    function key()
+    {
+        return key($this->array);
+    }
+
+    function valid()
+    {
+        return count($this->array) >= $this->index;
+    }
+
+    function rewind()
+    {
+        $this->index = 0;
+        return reset($this->array);
+    }
+
+    function next()
+    {
+        $this->index++;
+        return next($this->array);
+    }
+
+    function serialize()
+    {
+        return serialize($this->array);
+    }
+
+    function unserialize($str)
+    {
+        $this->array = unserialize($str);
+    }
+
+    function offsetGet($k)
+    {
+        return $this->array[$k];
+    }
+
+    function offsetSet($k, $v)
+    {
+        $this->array[$k] = $v;
+    }
+
+    function offsetUnset($k)
+    {
+        unset($this->array[$k]);
+    }
+
+    function offsetExists($k)
+    {
+        return isset($this->array[$k]);
     }
 
     function contains($val)
@@ -52,6 +110,11 @@ class ArrayObject implements \Countable
     function slice($offset, $lenth = null)
     {
         return new ArrayObject(array_slice($this->array, $offset, $lenth));
+    }
+
+    function rand()
+    {
+        return $this->array[array_rand($this->array, 1)];
     }
 
     function toArray()

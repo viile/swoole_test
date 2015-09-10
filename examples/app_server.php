d643f8b631ade0e55c831063bf6b047b95e42bf2
matyhtf <mikan.tenny@gmail.com>
2015-09-10 14:41:03 +0800

    增加指令管理

--- examples/app_server.php
+++ examples/app_server.php
@@ -5,7 +5,10 @@ require dirname(__DIR__) . '/libs/lib_config.php';
 
 //设置PID文件的存储路径
 Swoole\Network\Server::setPidFile(__DIR__ . '/app_server.pid');
-//显示Usage界面
+/**
+ * 显示Usage界面
+ * php app_server.php start|stop|reload
+ */
 Swoole\Network\Server::start(function ()
 {
     $server = Swoole\Protocol\WebServer::create(__DIR__ . '/swoole.ini');
